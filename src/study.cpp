497e4a168316fbbafe8cc840f8b2a0a1b4aa0a69
Christopher Dahl <christopher@dahl-online.com>
2019-10-06 01:48:55 +0100

    added LAB Comparison

--- src/study.cpp
+++ src/study.cpp
@@ -4,23 +4,25 @@
 #include <array>
 #include <list>
 #include <string>
-//#include <filesystem>
 #include <sys/stat.h>
 
-//namespace fs = std::filesystem;
 using namespace std;
 using namespace Magick;
 
-list < array < string, 8 >> csv = {
+list < array < string, 12 >> csv = {
   {
     "Image",
     "Depth",
     "ColorSpace",
     "Size",
-    "FuzzErrorMetric",
-    "MeanSquareErrorMetric",
-    "PeakSignalToRatioErrorMetric",
-    "PerceptualErrorMetric"
+    "RGBFuzzErrorMetric",
+    "RGBMeanSquareErrorMetric",
+    "RGBPeakSignalToRatioErrorMetric",
+    "RGBPerceptualErrorMetric",
+    "LABFuzzErrorMetric",
+    "LABMeanSquareErrorMetric",
+    "LABPeakSignalToRatioErrorMetric",
+    "LABPerceptualErrorMetric"
   }
 };
 
@@ -59,27 +61,6 @@ int depths[4] = {
   10
 };
 
-// Converts the Image to the given color space reduces the Bitdepth of the given 
-// Image and the increases the depth to the original depth after this the Image 
-//gets converted back to RGB
-Image convertReduceIncreasConvertback(Image image, int depth, ColorspaceType colorSpace){
-  ColorRGB px = image.pixelColor(1,1);
-  std::cout << px.red() << px.green() << px.blue() << std::endl;
-  
-  image.colorSpace(colorSpace);
-  image.modulusDepth(depth);
-  //image.depth(depth);
-  //image.depth(32);
-  //image.modulusDepth(32);
-  std::cout << image.colorSpace() << std::endl;
-  if (colorSpace != RGBColorspace){
-    image.colorSpace(RGBColorspace);
-  }
-  px = image.pixelColor(1,1);
-  std::cout << px.red() << px.green() << px.blue() << std::endl;
-  return image;
-}
-
 //calculates the size of the image
 int getImageSize(Image image, int depth, string colorSpace){
   int w = image.columns();
@@ -93,11 +74,34 @@ int getImageSize(Image image, int depth, string colorSpace){
   return size;
 }
 
+//saves the informations of the original image to the csv
+void imageInformationToCSV(Image image){
+  array < string, 12 > line;
+  line[0] = image.fileName(); //filename
+  line[1] = to_string(image.depth()); //depth
+  line[2] = "RGB(A)"; //colorspace
+  line[3] = to_string(image.fileSize()); //filesize
+  csv.push_back(line);
+}
+
+// Converts the Image to the given color space reduces the Bitdepth of the given 
+// Image and the increases the depth to the original depth after this the Image 
+//gets converted back to RGB
+Image convertReduceIncreasConvertback(Image image, int depth, ColorspaceType colorSpace, ColorspaceType conversionColorSpace){  
+  image.colorSpace(colorSpace);
+  image.modulusDepth(depth);
+  image.modulusDepth(32);
+  if (colorSpace != conversionColorSpace){
+    image.colorSpace(conversionColorSpace);
+  }
+  return image;
+}
+
 // compares the two images with (FuzzError, MeanSquaredErrorMetric, 
 // PeakSignalToNoiseRatioErrorMetric, PerceptualHashErrorMetric) and saves the results
 // in a csv document
-void compareImagesAndSaveInCSV(Image originalImage, Image changedImage, string colorSpace, int depth){
-  array < string, 8 > line;
+void compareImagesAndSaveInCSV(Image originalImage, Image changedImage, Image labImage, Image changedLabImage, string colorSpace, int depth){
+  array < string, 12 > line;
   line[0] = changedImage.fileName();
   line[1] = to_string(depth); //depth
   line[2] = colorSpace; //colorspace
@@ -106,99 +110,71 @@ void compareImagesAndSaveInCSV(Image originalImage, Image changedImage, string c
   line[5] = to_string(changedImage.compare(originalImage, MeanSquaredErrorMetric));
   line[6] = to_string(changedImage.compare(originalImage, PeakSignalToNoiseRatioErrorMetric));
   line[7] = to_string(changedImage.compare(originalImage, PerceptualHashErrorMetric));
-  csv.push_back(line);
-}
-
-//saves the informations of the original image to the csv
-void imageInformationToCSV(Image image){
-  array < string, 8 > line;
-  line[0] = image.fileName(); //filename
-  line[1] = to_string(image.depth()); //depth
-  line[2] = "RGB(A)"; //colorspace
-  line[3] = to_string(image.fileSize()); //filesize
+  line[8] = to_string(changedLabImage.compare(labImage, FuzzErrorMetric));
+  line[9] = to_string(changedLabImage.compare(labImage, MeanSquaredErrorMetric));
+  line[10] = to_string(changedLabImage.compare(labImage, PeakSignalToNoiseRatioErrorMetric));
+  line[11] = to_string(changedLabImage.compare(labImage, PerceptualHashErrorMetric));
   csv.push_back(line);
 }
 
 // compares the image with all color spaces and bit depth
-void compareColorSpaces(Image image, Image changedImage){
+void compareColorSpaces(Image image, Image changedImage, Image labImage, Image changedLabImage){
   imageInformationToCSV(image);
   for(int i = 0; i < 11; i++){
     for(int j = 0; j < 4; j++){
-      //Image changedImage = image;
-      changedImage = convertReduceIncreasConvertback(changedImage, depths[j], colorSpaces[i]);
-      compareImagesAndSaveInCSV(image, changedImage, colorSpaceNames[i], depths[j]);
+      changedImage = convertReduceIncreasConvertback(changedImage, depths[j], colorSpaces[i], RGBColorspace);
+      changedLabImage = convertReduceIncreasConvertback(changedLabImage, depths[j], colorSpaces[i], LabColorspace);
+      compareImagesAndSaveInCSV(image, changedImage, labImage, changedLabImage, colorSpaceNames[i], depths[j]);
     }
-  }
-}
-
-// saves the array in a csv file
-void saveToCSV(){
-  ofstream results("results.csv");
-  for (const array < string, 8 > & line: csv) {
-    for (const string & field: line) {
-      results << field << ",";
-    }
-    results << "\n";
   }
 }
 
 // reads in the Image and Compares them in diffrent color Spaces
 void readeInImageAndCompare(string path){
-  std::cout << "readeInImageAndCompare \n";
   Image image;
+  image.read(path);
   image.depth(32);
   image.modulusDepth(32);
-  image.read(path);
   Image image2;
+  image2.read(path);
   image2.depth(32);
   image2.modulusDepth(32);
-  image2.read(path);
-  std::cout << image.colorSpace() << std::endl;
-  compareColorSpaces(image, image2);
+  Image image3;
+  image3.read(path);
+  image3.colorSpace(LabColorspace);
+  image3.depth(32);
+  image3.modulusDepth(32);
+  Image image4;
+  image4.read(path);
+  image4.colorSpace(LabColorspace);
+  image4.depth(32);
+  image4.modulusDepth(32);
+  compareColorSpaces(image, image2, image3, image4);
 }
 
-// reads in all Images in the Folder and Compares them in diffrent color Spaces
-void readeInFolderAndCompareAll(string path){
-  //for (const auto & entry : fs::directory_iterator(path)){
-  //  std::cout << entry.path() << std::endl;
-  //  readeInImageAndCompare(entry.path());
-  //}
+// saves the array in a csv file
+void saveToCSV(){
+  ofstream results("results.csv");
+  for (const array < string, 12 > & line: csv) {
+    for (const string & field: line) {
+      results << field << ";";
+    }
+    results << "\n";
+  }
 }
 
 // handels input and starts the comparison of the images
 int main(int argc, char ** argv) {
   //case if the user gives no input
   if (argc < 2) {
-    cout << "Please pass: -pic ImagePath \n or: -all FolderPath \n";
+    cout << "Please input images to process.";
     return 0;
+  }else{
+        for (int i=1; i<argc; i++) {
+            readeInImageAndCompare(argv[i]);
+        }
+        saveToCSV();
+        return 0;
   }
-
-  string arg1 = argv[1];
-
-  //case if the user inputs -help -h
-  if (arg1 == "-h" || arg1 == "-help"){
-    cout << "Please pass: -pic ImagePath \n or: -all FolderPath \n";
-    return 0;
-  }
-
-  //normal case if the user inputs a file or a folder
-  if (arg1 == "-pic" && argc == 3){
-    readeInImageAndCompare(argv[2]);
-    saveToCSV();
-    return 0;
-  } else if(arg1 == "-all" && argc == 3) {
-    readeInFolderAndCompareAll(argv[2]);
-    saveToCSV();
-    return 0;
-  }else if(arg1 == "-comp" && argc == 4) {
-    Image image1;
-    image1.read(argv[2]);
-    Image image2;
-    image2.read(argv[3]);
-    compareImagesAndSaveInCSV(image1, image2, "", 32);
-    saveToCSV();
-    return 0;
-  }
-
   return -1;
 }
